88d2788a12bd6a7f73275f718f27157a5674f26c
Lucas Pazzim <lpazzim@gmail.com>
2020-10-21 23:10:07 -0300

    ajuste pesquisa + logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,39 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 import logo from '../../assets/Logo_ML2.png';
 import searchIcon from '../../assets/ic_Search.png';
-import Input from '../Input/Input.jsx'
+import Input from '../Input/Input.jsx';
 import './Header.scss';
 
 
 export default function Header(props) {
-    const [querySearch, setQuerySearch] = useState('');
-    const history = useHistory();
-    const limit = 4;
+  const [querySearch, setQuerySearch] = useState('');
+  const limit = 4;
 
-    function redirectToSearch(query) {
-        if (query) {
-            window.location.href = `${window.location.origin}/items?search=${query}&limit=${limit}`
-        }
+  function redirectToSearch(query) {
+    if (query) {
+      window.location.href = `${window.location.origin}/items?search=${query}&limit=${limit}`
     }
+  }
 
-    return (
-        <div className="header-container">
-            <div className="header">
-                <img src={logo} className="logo" alt="logo" />
-                <div className="input-search">
-                    <Input
-                        placeholder="Buscar produtos, marcas e muito mais…"
-                        maxLength={120}
-                        icon={searchIcon}
-                        onChange={(e) => setQuerySearch(e)}
-                        onClick={() => { redirectToSearch(querySearch) }
-                        }
-                    />
-                </div>
-            </div>
+  function redirectToHome() {
+    window.location.href = window.location.origin;
+  }
+
+  return (
+    <div className="header-container">
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" onClick={() => redirectToHome()} />
+        <div className="input-search">
+          <Input
+            placeholder="Buscar produtos, marcas e muito mais…"
+            maxLength={120}
+            icon={searchIcon}
+            onChange={(e) => setQuerySearch(e)}
+            onClick={() => { redirectToSearch(querySearch) }
+            }
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -17,6 +17,11 @@ export default function Input(props) {
     props.onChange(e);
   }
 
+  function handleKeyDown(e) {
+    if (e.keyCode === 13) {
+      props.onClick();
+    }
+  }
 
   return (
     <div className="input-container">
@@ -24,9 +29,10 @@ export default function Input(props) {
         placeholder={props.placeholder ? props.placeholder : ''}
         maxLength={props.maxLength ? props.maxLength : 100}
         onChange={(e) => { onChange(e.target.value) }}
+        onKeyDown={(e) => handleKeyDown(e)}
         type='text'
       />
-        <img src={props.icon ? props.icon : null} className="ico" alt="input-icon" onClick={() => onClick()} disabled={props.disabled} />
+      <img src={props.icon ? props.icon : null} className="ico" alt="input-icon" onClick={() => onClick()} disabled={props.disabled} />
     </div>
   );
 }
