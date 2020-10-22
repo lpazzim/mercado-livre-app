c36a61158a1c1ff79c058e3d872f9f820860a234
Lucas Pazzim <lpazzim@gmail.com>
2020-10-22 17:40:39 -0300

    express server config

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(process.env.PORT || 8080);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+} else {
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+  app.listen(process.env.PORT || 8080);
+}
